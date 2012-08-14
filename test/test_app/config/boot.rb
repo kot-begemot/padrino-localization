8b482031db3cf8cdf3a04d9d7a54e6f55c22d153
E-Max <max@studentify.nl>
2012-08-14 14:26:59 +0300

    Require padrion within tests

--- test/test_app/config/boot.rb
+++ test/test_app/config/boot.rb
@@ -1,12 +1,11 @@
 require 'bundler'
+Bundler.setup(:default, :test)
+require 'padrino'
+
 # Defines our constants
 PADRINO_ENV  = ENV["PADRINO_ENV"] ||= ENV["RACK_ENV"] ||= "development"  unless defined?(PADRINO_ENV)
 PADRINO_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-# Only have default and environemtn gems
-Bundler.setup(:default, :test)
-# Only require default and environment gems
-Bundler.require(:default, :test)
 
 ##
 # Add here your before load hooks
